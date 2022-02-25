8c3d9aeb1fdd9e237f9fec485a10a09a7c3b918a
bstefano79 <bstefano79@gmail.com>
2022-02-25 11:29:24 +0000

    costruzione della routing

--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ app.get("/", function(req, res){
     console.log("hai fatto una get");
 });
 
+app.get("/pagina", function(req, res){
+    res.send("<h1>SEI SU PAGINA</h1>");
+    console.log("hai fatto una get su pagina");
+});
+
+
 app.listen(3000, function(){
     console.log("Server partito sulla porta 3000!!!");
 });
